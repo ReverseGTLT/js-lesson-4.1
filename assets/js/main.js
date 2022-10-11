6b8258dc8583ba3f26602787b3395c757a74b6c5
ReverseGTLT <dzewrpifzc@gmail.com>
2022-10-12 00:03:08 +0300

    finish

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+let firstNum = +prompt('Enter first number');
+let secondNum = +prompt('Enter second number');
+
+let sum = firstNum + secondNum;
+let dif = firstNum - secondNum;
+let multiple = firstNum * secondNum;
+let div = firstNum / secondNum;
+
+alert(`${firstNum}+${secondNum}=${sum} \n${firstNum}-${secondNum}=${dif} \n${firstNum}*${secondNum}=${multiple} \n${firstNum}/${secondNum}=${div}`);
